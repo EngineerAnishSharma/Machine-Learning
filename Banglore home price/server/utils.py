83db6f0a9dd9851ae9e91ff720fd9478aa30380c
Anish <engineeranish10000@gmail.com>
2024-02-05 01:03:34 +0530

    added ui

--- Banglore home price/server/utils.py
+++ Banglore home price/server/utils.py
@@ -45,8 +45,8 @@ def get_data_columns():
 
 if __name__ == '__main__':
     load_saved_artifacts()
-    print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+    # print(get_location_names())
+    # print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    # print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    # print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    # print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
